6b7afad21ff5bc28a490ffd268457418a6bb0f36
Ravi Kiran <allimilliravikiran@gmail.com>
2026-05-10 17:46:04 +0530

    added db port

--- config/mysql.js
+++ config/mysql.js
@@ -4,11 +4,13 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const db = mysql.createConnection({
+
+const db =  mysql.createConnection({
     host: process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port:process.env.PORT,
 });
 
 db.connect((error) =>{
